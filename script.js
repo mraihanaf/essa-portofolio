ce7c80b71cbe1e25d97ac654fd2bb28c63d8deab
mraihanaf <mraihanaf0@gmail.com>
2024-07-31 13:47:06 +0700

    feat: add scroll event

---
+++ script.js
@@ -0,0 +1,19 @@
+let lastKnownScrollPosition = 0;
+let ticking = false;
+function doSomething(scrollPos) {
+  // Do something with the scroll position
+//   console.log(scrollPos)
+}
+
+document.addEventListener("scroll", (event) => {
+  lastKnownScrollPosition = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      doSomething(lastKnownScrollPosition);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
